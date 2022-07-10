6ac2a7cd500545c7b83156df79dab464d6ddb08b
Viktor Glebov <glebov_v94@mail.ru>
2022-07-10 21:25:13 +0400

    Fix code

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,27 +2,16 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first;
+        int second;
+        int third;
+
+        first = 1;
+        second = 10;
+        third = 100;
 
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
-
-        int linkToFirst = first;
-        int linkToSecond = second;
-        int linkToThird = third;
-
-        first = 15;
-        second = 6;
-        third = 4;
-
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-        System.out.println(linkToFirst);
-        System.out.println(linkToSecond);
-        System.out.println(linkToThird);
     }
 }
